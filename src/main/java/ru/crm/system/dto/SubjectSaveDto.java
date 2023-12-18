48bfabb167097d5fc241656268822467946ccbac
Vasili Bandarenka <darken123@inbox.ru>
2023-12-18 20:15:57 +0300

    Добавил репозиторий, сервис, маппер, dto  для Subject

---
+++ src/main/java/ru/crm/system/dto/SubjectSaveDto.java
@@ -0,0 +1,27 @@
+package ru.crm.system.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import ru.crm.system.database.entity.Student;
+import ru.crm.system.database.entity.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DTO приходит с фронта для сохранения в базу данных,
+ * без id ,он присваивается базой данных
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SubjectSaveDto {
+
+    String name;
+
+    List<Student> students = new ArrayList<>();
+
+    List<Teacher> teachers = new ArrayList<>();
+}
---
+++ src/main/java/ru/crm/system/mapper/SubjectMapper.java
@@ -0,0 +1,56 @@
+package ru.crm.system.mapper;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.crm.system.database.entity.Subject;
+import ru.crm.system.dto.SubjectDto;
+import ru.crm.system.dto.SubjectSaveDto;
+
+import java.util.List;
+
+/**
+ * Класс для маппинга в дто и обратно
+ * единичные сущности и list дто
+ */
+@Component
+@AllArgsConstructor
+public class SubjectMapper {
+
+    public Subject fromDto(SubjectDto dto) {
+        return Subject.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .students(dto.getStudents())
+                .teachers(dto.getTeachers())
+                .build();
+    }
+
+    public Subject fromDto(SubjectSaveDto dto) {
+        return Subject.builder()
+                .name(dto.getName())
+                .students(dto.getStudents())
+                .teachers(dto.getTeachers())
+                .build();
+    }
+
+    public List<Subject> fromDto(List<SubjectDto> dtolist) {
+        return dtolist.stream()
+                .map(this::fromDto)
+                .toList();
+    }
+
+    public SubjectDto toDto(Subject subject) {
+        return SubjectDto.builder()
+                .id(subject.getId())
+                .name(subject.getName())
+                .students(subject.getStudents())
+                .teachers(subject.getTeachers())
+                .build();
+    }
+
+    public List<SubjectDto> toDto(List<Subject> subjectList) {
+        return subjectList.stream()
+                .map(this::toDto)
+                .toList();
+    }
+}
