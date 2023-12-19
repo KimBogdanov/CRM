2b3c3b6aa3553fc0bbceba98d71463488d916b45
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-19 09:45:57 +0800

    feat: change OrderStatus values

--- src/main/java/ru/crm/system/database/entity/enums/OrderStatus.java
+++ src/main/java/ru/crm/system/database/entity/enums/OrderStatus.java
@@ -1,10 +1,12 @@
 package ru.crm.system.database.entity.enums;
 
 public enum OrderStatus {
-    NOT_PROCESSED,
-    SET_APPOINTMENT,
-    SUCCESSFUL_MEETING,
+    UNPROCESSED,
+    SCHEDULE_APPOINTMENT,
+    APPOINTMENT_SCHEDULED,
+    APPOINTMENT_COMPLETED,
     SUCCESSFULLY_COMPLETED,
-    REFUSED,
-    DEAD
+    POOR_LEAD,
+    REFUSED_TO_PURCHASE,
+    RESERVED
 }
