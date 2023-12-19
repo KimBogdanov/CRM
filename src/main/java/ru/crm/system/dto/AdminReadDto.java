6f7b1a613cc1606a86edbe42806f0cdce0e5932c
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-19 14:23:43 +0800

    feat: add dto to create and read Admin

---
+++ src/main/java/ru/crm/system/dto/AdminReadDto.java
@@ -0,0 +1,17 @@
+package ru.crm.system.dto;
+
+import lombok.Builder;
+import lombok.experimental.FieldNameConstants;
+
+import java.math.BigDecimal;
+
+@Builder
+@FieldNameConstants
+public record AdminReadDto(Integer id,
+                           String firstName,
+                           String lastName,
+                           String phone,
+                           String email,
+                           String avatar,
+                           BigDecimal shiftRate) {
+}
