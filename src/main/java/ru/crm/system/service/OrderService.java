5d3cd3a4f5479d7b0ff3e8f052abf0b665eb5614
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-20 18:10:48 +0800

    feat: change create() at OrderService

--- src/main/java/ru/crm/system/service/OrderService.java
+++ src/main/java/ru/crm/system/service/OrderService.java
@@ -8,9 +8,11 @@ import ru.crm.system.database.entity.enums.ActionType;
 import ru.crm.system.database.repository.OrderRepository;
 import ru.crm.system.dto.LogInfoCreateDto;
 import ru.crm.system.dto.OrderCreateEditDto;
+import ru.crm.system.dto.OrderReadDto;
 import ru.crm.system.listener.entity.AccessType;
 import ru.crm.system.listener.entity.EntityEvent;
 import ru.crm.system.mapper.OrderCreateEditMapper;
+import ru.crm.system.mapper.OrderReadMapper;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -23,25 +25,26 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderCreateEditMapper orderCreatedEditMapper;
     private final ApplicationEventPublisher publisher;
+    private final OrderReadMapper orderReadMapper;
 
     @Transactional
-    public Integer create(OrderCreateEditDto createDto) {
+    public OrderReadDto create(OrderCreateEditDto createDto) {
         return Optional.of(createDto)
                 .map(orderCreatedEditMapper::map)
                 .map(orderRepository::save)
                 .map(order -> {
                     var logInfo = createLogInfo(order.getId());
+                    logInfo.setAction(ActionType.CREATED);
                     publisher.publishEvent(new EntityEvent<>(order, AccessType.CREATE, logInfo));
-                    return order.getId();
+                    return orderReadMapper.map(order);
                 })
                 .orElseThrow();
     }
 
-    private LogInfoCreateDto createLogInfo(Integer id) {
+    private LogInfoCreateDto createLogInfo(Integer orderId) {
         return LogInfoCreateDto.builder()
-                .action(ActionType.CREATED)
                 .createdAt(LocalDateTime.now())
-                .orderId(id)
+                .orderId(orderId)
                 .build();
     }
 }
