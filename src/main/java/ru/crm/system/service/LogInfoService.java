23528104c15d3e4acec2010bdf395d6b3bc8fea5
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-20 13:23:22 +0800

    feat: add LogInfoRepository and LogInfoService with create()

---
+++ src/main/java/ru/crm/system/service/LogInfoService.java
@@ -0,0 +1,28 @@
+package ru.crm.system.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.crm.system.database.entity.LogInfo;
+import ru.crm.system.database.repository.LogInfoRepository;
+import ru.crm.system.dto.LogInfoCreateDto;
+import ru.crm.system.mapper.LogInfoCreateMapper;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LogInfoService {
+
+    private final LogInfoRepository logInfoRepository;
+    private final LogInfoCreateMapper logInfoCreateMapper;
+
+    public Integer create(LogInfoCreateDto createDto) {
+        return Optional.of(createDto)
+                .map(logInfoCreateMapper::map)
+                .map(logInfoRepository::save)
+                .map(LogInfo::getId)
+                .orElseThrow();
+    }
+}
