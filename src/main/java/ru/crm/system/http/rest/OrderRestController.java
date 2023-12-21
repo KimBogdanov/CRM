a9b377430f2231dd13eefcbdb148669a4dc99627
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-21 16:01:37 +0800

    feat: add findAll() at OrderRestController

--- src/main/java/ru/crm/system/http/rest/OrderRestController.java
+++ src/main/java/ru/crm/system/http/rest/OrderRestController.java
@@ -3,14 +3,18 @@ package ru.crm.system.http.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import ru.crm.system.database.entity.enums.OrderStatus;
 import ru.crm.system.dto.order.OrderCreateEditDto;
 import ru.crm.system.dto.order.OrderReadDto;
 import ru.crm.system.service.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -23,5 +27,21 @@ public class OrderRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public OrderReadDto create(@Validated @RequestBody OrderCreateEditDto createDto) {
         return orderService.create(createDto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderReadDto> getAll() {
+        return orderService.findAll();
+    }
+
+    /**
+     * Метод для получения всех возможных статусов заказа,
+     * чтобы было можно менять статус при перетаскивании заказа в окне заказов.
+     */
+    @GetMapping("/status")
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderStatus> getStatuses() {
+        return List.of(OrderStatus.values());
     }
 }
