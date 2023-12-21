6a5746ff4124e55a56d8808aafe01b673dd592d8
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-21 14:00:12 +0800

    feat: add changeStatus() at OrderService

--- src/main/java/ru/crm/system/service/OrderService.java
+++ src/main/java/ru/crm/system/service/OrderService.java
@@ -4,18 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.crm.system.database.entity.Comment;
 import ru.crm.system.database.entity.enums.ActionType;
+import ru.crm.system.database.entity.enums.OrderStatus;
 import ru.crm.system.database.repository.OrderRepository;
-import ru.crm.system.dto.LogInfoCreateDto;
-import ru.crm.system.dto.OrderCreateEditDto;
-import ru.crm.system.dto.OrderReadDto;
+import ru.crm.system.dto.loginfo.LogInfoCreateDto;
+import ru.crm.system.dto.order.OrderCreateEditDto;
+import ru.crm.system.dto.order.OrderReadDto;
 import ru.crm.system.listener.entity.AccessType;
 import ru.crm.system.listener.entity.EntityEvent;
 import ru.crm.system.mapper.OrderCreateEditMapper;
 import ru.crm.system.mapper.OrderReadMapper;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 @Service
 @RequiredArgsConstructor
@@ -41,10 +45,38 @@ public class OrderService {
                 .orElseThrow();
     }
 
+    @Transactional
+    public OrderReadDto changeStatus(Integer orderId,
+                                     OrderStatus status,
+                                     Integer adminId,
+                                     String description) {
+        return Optional.ofNullable(orderId)
+                .flatMap(orderRepository::findById)
+                .map(order -> {
+                    var logInfo = createLogInfo(order.getId());
+                    setLogInfo(adminId, description, logInfo);
+                    publisher.publishEvent(new EntityEvent<>(order, AccessType.CHANGE_STATUS, logInfo));
+                    order.setStatus(status);
+                    order.addComment(createComment());
+                    var changedOrder = orderRepository.saveAndFlush(order);
+                    return orderReadMapper.map(changedOrder);
+                }).orElseThrow();
+    }
+
     private LogInfoCreateDto createLogInfo(Integer orderId) {
         return LogInfoCreateDto.builder()
-                .createdAt(LocalDateTime.now())
+                .createdAt(now())
                 .orderId(orderId)
                 .build();
+    }
+
+    private Comment createComment() {
+        return Comment.of("Новый заказ", now().truncatedTo(SECONDS));
+    }
+
+    private static void setLogInfo(Integer adminId, String description, LogInfoCreateDto logInfo) {
+        logInfo.setAction(ActionType.COMMENT);
+        logInfo.setAdminId(adminId);
+        logInfo.setDescription(description);
     }
 }
