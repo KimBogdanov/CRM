aa85715097f3c7bfe4706a868bc087dfd910078f
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-22 17:10:46 +0800

    feat: change comment table at schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,10 +27,10 @@ CREATE TABLE IF NOT EXISTS orders
 
 CREATE TABLE IF NOT EXISTS comment
 (
+    order_id INT REFERENCES orders (id),
     text     VARCHAR(256),
     added_at TIMESTAMP(0) NOT NULL,
-    order_id INT REFERENCES orders (id),
-    PRIMARY KEY (added_at, order_id)
+    PRIMARY KEY (order_id, text, added_at)
 );
 -- rollback DROP TABLE comment;
 
