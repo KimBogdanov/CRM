dfa173da6f884d4e3a338b89b3bb3433aa69f3b6
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-23 00:04:03 +0800

    feat: add method to find Subject by name

--- src/main/java/ru/crm/system/database/repository/SubjectRepository.java
+++ src/main/java/ru/crm/system/database/repository/SubjectRepository.java
@@ -1,7 +1,15 @@
 package ru.crm.system.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.crm.system.database.entity.Subject;
 
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+
+    @Query("""
+            select s
+            from Subject s
+            where s.name = :name
+            """)
+    Subject getSubjectByName(String name);
 }
