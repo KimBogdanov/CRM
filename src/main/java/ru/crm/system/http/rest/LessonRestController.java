43a6f792c84805689719211ed7b43f45f6b3de11
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-24 18:34:15 +0800

    feat: add GET at LessonRestController

--- src/main/java/ru/crm/system/http/rest/LessonRestController.java
+++ src/main/java/ru/crm/system/http/rest/LessonRestController.java
@@ -2,6 +2,7 @@ package ru.crm.system.http.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,13 @@ import ru.crm.system.service.LessonService;
 public class LessonRestController {
 
     private final LessonService lessonService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LessonReadDto findById(@PathVariable Integer id) {
+        return lessonService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
