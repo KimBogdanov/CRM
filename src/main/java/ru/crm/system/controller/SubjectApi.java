4dda3da3fe91cb6df9966becb8aba23d400c9963
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-24 20:09:19 +0800

    refactor: refactor SubjectApi

--- src/main/java/ru/crm/system/controller/SubjectApi.java
+++ src/main/java/ru/crm/system/controller/SubjectApi.java
@@ -1,7 +1,13 @@
 package ru.crm.system.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.crm.system.database.entity.Subject;
 import ru.crm.system.dto.SubjectDto;
 import ru.crm.system.dto.SubjectSaveDto;
@@ -10,7 +16,7 @@ import ru.crm.system.service.SubjectService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/subjects")
+@RequestMapping("/api/v1/subjects")
 public class SubjectApi {
 
     private final SubjectService subjectService;
