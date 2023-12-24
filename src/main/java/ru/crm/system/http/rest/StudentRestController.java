e67f6eb8a90cd0581a13fbc959746d6e2581dc42
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-24 18:38:43 +0800

    feat: add GET findById() at StudentRestController

--- src/main/java/ru/crm/system/http/rest/StudentRestController.java
+++ src/main/java/ru/crm/system/http/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package ru.crm.system.http.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,12 @@ import ru.crm.system.service.StudentService;
 public class StudentRestController {
 
     private final StudentService studentService;
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public StudentReadDto findById(@PathVariable Integer id) {
+        return studentService.findById(id);
+    }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
