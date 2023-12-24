465f15fc2c3be83cca5efb17bdfd86642a53f7d0
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-24 14:11:26 +0800

    feat: add method to add money into balance

--- src/main/java/ru/crm/system/service/AbonementService.java
+++ src/main/java/ru/crm/system/service/AbonementService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.crm.system.database.entity.Abonement;
 import ru.crm.system.database.entity.enums.ActionType;
 import ru.crm.system.database.repository.AbonementRepository;
 import ru.crm.system.dto.abonement.AbonementCreatEditDto;
@@ -14,6 +15,7 @@ import ru.crm.system.listener.entity.EntityEvent;
 import ru.crm.system.mapper.abonement.AbonementCreateEditMapper;
 import ru.crm.system.mapper.abonement.AbonementReadMapper;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import static java.time.LocalDateTime.now;
@@ -49,6 +51,40 @@ public class AbonementService {
                 .orElseThrow();
     }
 
+    @Transactional
+    public Optional<AbonementReadDto> addMoneyIntoBalance(Integer adminId,
+                                                          Integer abonementId,
+                                                          BigDecimal moneyToAdd) {
+        return abonementRepository.findById(abonementId)
+                .map(abonement -> {
+                    var newBalance = addMoney(moneyToAdd, abonement);
+                    LogInfoCreateDto logInfo = createAddMoneyLogInfo(adminId, moneyToAdd, abonement, newBalance);
+                    publisher.publishEvent(new EntityEvent<>(abonement, AccessType.UPDATE, logInfo));
+                    return abonement;
+                })
+                .map(abonementReadMapper::map);
+    }
+
+    /**
+     * Метод для создания LogInfo при зачислении денег на баланс ученика.
+     */
+    private LogInfoCreateDto createAddMoneyLogInfo(Integer adminId,
+                                                   BigDecimal moneyToAdd,
+                                                   Abonement abonement,
+                                                   BigDecimal newBalance) {
+        return LogInfoCreateDto.builder()
+                .action(ActionType.ADD_MONEY_INTO_STUDENT_BALANCE)
+                .description(String.format("На счёт ученика %s %s внесено %.2f руб. Текущий балан %s руб.",
+                        abonement.getStudent().getUserInfo().getFirstName(),
+                        abonement.getStudent().getUserInfo().getLastName(),
+                        moneyToAdd,
+                        newBalance))
+                .createdAt(now().truncatedTo(SECONDS))
+                .adminId(adminId)
+                .teacherId(abonement.getId())
+                .build();
+    }
+
     /**
      * Метод для создания базового LogInfo для всех методов
      */
@@ -57,5 +93,13 @@ public class AbonementService {
                 .createdAt(now().truncatedTo(SECONDS))
                 .orderId(orderId)
                 .build();
+    }
+
+    private BigDecimal addMoney(BigDecimal moneyToAdd, Abonement abonement) {
+        var balance = abonement.getBalance();
+        var newBalance = balance.add(moneyToAdd);
+        abonement.setBalance(newBalance);
+        abonementRepository.flush();
+        return newBalance;
     }
 }
