face87ea155383a7f4ef55abca097deda281f1f5
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-25 13:25:47 +0800

    feat: add POST creat() at TeacherRestController

--- src/main/java/ru/crm/system/http/rest/TeacherRestController.java
+++ src/main/java/ru/crm/system/http/rest/TeacherRestController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import ru.crm.system.dto.teacher.TeacherCreateEditDto;
 import ru.crm.system.dto.teacher.TeacherReadDto;
 import ru.crm.system.service.TeacherService;
 
@@ -23,5 +25,11 @@ public class TeacherRestController {
     public TeacherReadDto findById(@PathVariable Integer id) {
         return teacherService.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TeacherReadDto create(TeacherCreateEditDto createDto) {
+        return teacherService.create(createDto);
     }
 }
