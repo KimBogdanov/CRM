cb40ea23f4cd6591ad6f95c0a3a62c5252259dc7
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-25 11:36:57 +0800

    feat: add GET findById() at TeacherRestController

---
+++ src/main/java/ru/crm/system/http/rest/TeacherRestController.java
@@ -0,0 +1,27 @@
+package ru.crm.system.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import ru.crm.system.dto.teacher.TeacherReadDto;
+import ru.crm.system.service.TeacherService;
+
+@RestController
+@RequestMapping("/api/v1/teachers")
+@RequiredArgsConstructor
+public class TeacherRestController {
+
+    private final TeacherService teacherService;
+
+    @GetMapping("/{id})")
+    @ResponseStatus(HttpStatus.OK)
+    public TeacherReadDto findById(@PathVariable Integer id) {
+        return teacherService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
