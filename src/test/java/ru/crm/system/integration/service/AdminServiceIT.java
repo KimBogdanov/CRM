553786556a6e92f4bb5a34c13c56099c0befbd8c
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-25 17:08:12 +0800

    feat: add AdminTestIT

---
+++ src/test/java/ru/crm/system/integration/service/AdminServiceIT.java
@@ -0,0 +1,56 @@
+package ru.crm.system.integration.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import ru.crm.system.dto.admin.AdminCreateEditDto;
+import ru.crm.system.integration.IT;
+import ru.crm.system.service.AdminService;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@IT
+@RequiredArgsConstructor
+public class AdminServiceIT {
+
+    private static final Integer EXISTING_ADMIN_ID = 1;
+
+    private final AdminService adminService;
+
+    @Test
+    void create_shouldSaveAdminToDb_whenCreateDtoValid() {
+        var adminCreateDto = getAdminCreateDto();
+
+        var actualAdmin = adminService.create(adminCreateDto);
+        assertThat(actualAdmin.id()).isPositive();
+    }
+
+    @Test
+    void findById_shouldFindById_ifAdminExists() {
+        var actualAdmin = adminService.findById(EXISTING_ADMIN_ID);
+
+        assertThat(actualAdmin).isPresent();
+
+        actualAdmin.ifPresent(admin ->
+                assertAll(() -> {
+                    assertThat(admin.id()).isEqualTo(EXISTING_ADMIN_ID);
+                    assertThat(admin.firstName()).isEqualTo("Андрей");
+                    assertThat(admin.lastName()).isEqualTo("Админов");
+                    assertThat(admin.phone()).isEqualTo("8-925-999-99-99");
+                    assertThat(admin.email()).isEqualTo("adminFirst@gmail.com");
+                }));
+    }
+
+    private AdminCreateEditDto getAdminCreateDto() {
+        return AdminCreateEditDto.builder()
+                .firstName("AdminFirstName")
+                .lastName("AdminLastName")
+                .phone("8-924-775-58-58")
+                .email("admin@gmail.com")
+                .rawPassword("rawPassword")
+                .shiftRate(BigDecimal.valueOf(2500))
+                .build();
+    }
+}
