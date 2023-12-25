f6e382921ec781a2e725c4ce2dbdb459b4d90b9b
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-25 13:04:20 +0800

    feat: add create() at TeacherService

--- src/main/java/ru/crm/system/service/TeacherService.java
+++ src/main/java/ru/crm/system/service/TeacherService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.crm.system.database.repository.TeacherRepository;
+import ru.crm.system.dto.teacher.TeacherCreateEditDto;
 import ru.crm.system.dto.teacher.TeacherReadDto;
+import ru.crm.system.mapper.teacher.TeacherCreateEditMapper;
 import ru.crm.system.mapper.teacher.TeacherReadMapper;
 
 import java.util.Optional;
@@ -16,9 +18,19 @@ public class TeacherService {
 
     private final TeacherRepository teacherRepository;
     private final TeacherReadMapper teacherReadMapper;
+    private final TeacherCreateEditMapper teacherCreateEditMapper;
 
     public Optional<TeacherReadDto> findById(Integer id) {
         return teacherRepository.findById(id)
                 .map(teacherReadMapper::map);
+    }
+
+    @Transactional
+    public TeacherReadDto create(TeacherCreateEditDto createDto) {
+        return Optional.of(createDto)
+                .map(teacherCreateEditMapper::map)
+                .map(teacherRepository::save)
+                .map(teacherReadMapper::map)
+                .orElseThrow(RuntimeException::new);
     }
 }
