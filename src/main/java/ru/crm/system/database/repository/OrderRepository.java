3affe64828aff9b9645a04ed89e332164da6731a
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-26 11:18:32 +0800

    feat: add GET getStatuses() to get all OrderStatus

--- src/main/java/ru/crm/system/database/repository/OrderRepository.java
+++ src/main/java/ru/crm/system/database/repository/OrderRepository.java
@@ -1,8 +1,17 @@
 package ru.crm.system.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import ru.crm.system.database.entity.Order;
+import ru.crm.system.database.entity.enums.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
+    @Modifying
+    @Query("""
+            update Order o
+            set o.status = :status
+            """)
+    void changeStatus(OrderStatus status);
 }
