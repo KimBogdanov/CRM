67e0aa85572e17ccd9977d521013af51a10e0bb1
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-12-29 09:51:49 +0800

    feat: change mappers for Order

--- src/main/java/ru/crm/system/mapper/OrderCreateEditMapper.java
+++ src/main/java/ru/crm/system/mapper/OrderCreateEditMapper.java
@@ -2,35 +2,48 @@ package ru.crm.system.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.crm.system.database.entity.Admin;
 import ru.crm.system.database.entity.Order;
+import ru.crm.system.database.repository.AdminRepository;
 import ru.crm.system.dto.order.OrderCreateEditDto;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class OrderCreateEditMapper implements Mapper<OrderCreateEditDto, Order> {
 
+    private final AdminRepository adminRepository;
+
     @Override
     public Order map(OrderCreateEditDto createDto) {
         return Order.builder()
-                .status(createDto.status())
-                .orderName(createDto.orderName())
-                .clientName(createDto.clientName())
-                .phone(createDto.phone())
+                .status(createDto.getStatus())
+                .orderName(createDto.getOrderName())
+                .clientName(createDto.getClientName())
+                .phone(createDto.getPhone())
                 .createdAt(LocalDateTime.now())
-                .requestSource(createDto.requestSource())
+                .requestSource(createDto.getRequestSource())
+                .admin(getAdmin(createDto.getAdminId()))
                 .build();
     }
 
     @Override
     public Order map(OrderCreateEditDto editDto, Order entity) {
-        entity.setStatus(editDto.status());
-        entity.setOrderName(editDto.orderName());
-        entity.setClientName(editDto.clientName());
-        entity.setPhone(editDto.phone());
-        entity.setCreatedAt(editDto.createdAt());
-        entity.setRequestSource(editDto.requestSource());
+        entity.setStatus(editDto.getStatus());
+        entity.setOrderName(editDto.getOrderName());
+        entity.setClientName(editDto.getClientName());
+        entity.setPhone(editDto.getPhone());
+        entity.setCreatedAt(editDto.getCreatedAt());
+        entity.setRequestSource(editDto.getRequestSource());
+        entity.setAdmin(getAdmin(editDto.getAdminId()));
         return entity;
+    }
+
+    private Admin getAdmin(Integer id) {
+        return Optional.ofNullable(id)
+                .flatMap(adminRepository::findById)
+                .orElse(null);
     }
 }
