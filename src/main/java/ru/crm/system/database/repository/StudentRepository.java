d4090d735420a2c585893481a80b3538744cfd14
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-27 20:17:53 +0800

    feat: add method to find all students by its subject

--- src/main/java/ru/crm/system/database/repository/StudentRepository.java
+++ src/main/java/ru/crm/system/database/repository/StudentRepository.java
@@ -1,7 +1,18 @@
 package ru.crm.system.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.crm.system.database.entity.Student;
+import ru.crm.system.database.entity.Subject;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    @Query("""
+            select Student from Student
+            left join Subject on Student.subject
+            where Subject.name =:subject
+            """)
+    List<Student> findAllBySubject(Subject subject);
 }
