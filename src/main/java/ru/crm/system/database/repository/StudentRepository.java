8438819a90194d165c35dda1a0a3a1de9eca3918
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-28 17:47:54 +0800

    fix: change findAllBySubject at StudentRepository

--- src/main/java/ru/crm/system/database/repository/StudentRepository.java
+++ src/main/java/ru/crm/system/database/repository/StudentRepository.java
@@ -3,16 +3,15 @@ package ru.crm.system.database.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.crm.system.database.entity.Student;
-import ru.crm.system.database.entity.Subject;
 
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     @Query("""
-            select Student from Student
-            left join Subject on Student.subject
-            where Subject.name =:subject
+            select st from Student st
+            left join Subject sj on st.subject.id = sj.id
+            where sj.name =: subject
             """)
-    List<Student> findAllBySubject(Subject subject);
+    List<Student> findAllBySubject(String subject);
 }
