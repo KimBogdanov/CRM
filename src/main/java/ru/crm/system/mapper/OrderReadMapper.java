7b5b01464554dc00d204296be580f7b23bb81c05
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-02-01 12:46:24 +0800

    feat: add admin mapping in Order read and create mapper

--- src/main/java/ru/crm/system/mapper/OrderReadMapper.java
+++ src/main/java/ru/crm/system/mapper/OrderReadMapper.java
@@ -22,6 +22,7 @@ public class OrderReadMapper implements Mapper<Order, OrderReadDto> {
                 .phone(entity.getPhone())
                 .requestSource(entity.getRequestSource())
                 .createdAt(dateTime)
+                .adminId(entity.getAdmin() == null ? null : entity.getAdmin().getId())
                 .build();
     }
 }
