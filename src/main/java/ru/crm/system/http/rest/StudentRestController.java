65d3127955ebed1856a2bae9c3a2daa2e0285f4b
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-02-09 19:34:33 +0800

    feat: add endpoint to get students by subject

--- src/main/java/ru/crm/system/http/rest/StudentRestController.java
+++ src/main/java/ru/crm/system/http/rest/StudentRestController.java
@@ -3,18 +3,22 @@ package ru.crm.system.http.rest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.crm.system.dto.student.StudentCreateEditDto;
 import ru.crm.system.dto.student.StudentReadDto;
 import ru.crm.system.exception.NotFoundException;
 import ru.crm.system.service.StudentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -38,5 +42,12 @@ public class StudentRestController {
                                  Integer adminId,
                                  @Validated @RequestBody StudentCreateEditDto createDto) {
         return studentService.create(orderId, createDto, adminId);
+    }
+
+    @GetMapping("/by-subject")
+    @Operation(summary = "Method to find students by subject.")
+    public ResponseEntity<List<StudentReadDto>> findAllBySubject(@RequestParam String subject) {
+        var studentsBySubject = studentService.findAllBySubject(subject);
+        return ResponseEntity.ok().body(studentsBySubject);
     }
 }
