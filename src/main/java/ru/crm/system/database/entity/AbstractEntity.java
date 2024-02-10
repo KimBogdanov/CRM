905ad3eae7d01b5dae169b93281b880c026f9163
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-02-10 17:26:56 +0800

    feat: add create and modify data auditing

--- src/main/java/ru/crm/system/database/entity/AbstractEntity.java
+++ src/main/java/ru/crm/system/database/entity/AbstractEntity.java
@@ -4,20 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractEntity implements BaseEntity<Integer> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @CreatedDate
+    private Instant savedAt;
+
+    @LastModifiedDate
+    private Instant modifiedAt;
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS admin
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(128)  NOT NULL,
-    last_name  VARCHAR(128)  NOT NULL,
-    phone      VARCHAR(32)   NOT NULL UNIQUE,
-    email      VARCHAR(128)  NOT NULL UNIQUE,
-    password   VARCHAR(64)   NOT NULL,
-    role       VARCHAR(32)   NOT NULL,
-    shift_rate NUMERIC(8, 2) NOT NULL,
-    avatar     VARCHAR(128)
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(128)  NOT NULL,
+    last_name   VARCHAR(128)  NOT NULL,
+    phone       VARCHAR(32)   NOT NULL UNIQUE,
+    email       VARCHAR(128)  NOT NULL UNIQUE,
+    password    VARCHAR(64)   NOT NULL,
+    role        VARCHAR(32)   NOT NULL,
+    shift_rate  NUMERIC(8, 2) NOT NULL,
+    avatar      VARCHAR(128),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS orders
@@ -20,26 +22,32 @@ CREATE TABLE IF NOT EXISTS orders
     phone          VARCHAR(32)  NOT NULL,
     request_source VARCHAR(512),
     created_at     TIMESTAMP(0) NOT NULL,
-    admin_id       INT REFERENCES admin (id)
+    admin_id       INT REFERENCES admin (id),
+    saved_at       TIMESTAMP,
+    modified_at    TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS subject
 (
-    id   INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL UNIQUE
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(64) NOT NULL UNIQUE,
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS student
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(128) NOT NULL,
-    last_name  VARCHAR(128),
-    phone      VARCHAR(32)  NOT NULL,
-    email      VARCHAR(128) UNIQUE,
-    password   VARCHAR(64),
-    role       VARCHAR(32)  NOT NULL,
-    subject_id INT REFERENCES subject (id),
-    avatar     VARCHAR(128)
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(128) NOT NULL,
+    last_name   VARCHAR(128),
+    phone       VARCHAR(32)  NOT NULL,
+    email       VARCHAR(128) UNIQUE,
+    password    VARCHAR(64),
+    role        VARCHAR(32)  NOT NULL,
+    subject_id  INT REFERENCES subject (id),
+    avatar      VARCHAR(128),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS teacher
@@ -54,7 +62,9 @@ CREATE TABLE IF NOT EXISTS teacher
     avatar          VARCHAR(128),
     salary_per_hour NUMERIC(10, 2),
     status          VARCHAR(32)  NOT NULL,
-    pay_ratio       DOUBLE
+    pay_ratio       DOUBLE,
+    saved_at        TIMESTAMP,
+    modified_at     TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS log_info
@@ -67,7 +77,9 @@ CREATE TABLE IF NOT EXISTS log_info
     order_id    INT REFERENCES orders (id),
     student_id  INT REFERENCES student (id),
     teacher_id  INT REFERENCES teacher (id),
-    lesson_id   INT REFERENCES lesson (id)
+    lesson_id   INT REFERENCES lesson (id),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS abonement
@@ -79,7 +91,9 @@ CREATE TABLE IF NOT EXISTS abonement
     begin             DATE,
     expire            DATE,
     status            VARCHAR(64) NOT NULL,
-    student_id        INT REFERENCES student (id)
+    student_id        INT REFERENCES student (id),
+    saved_at          TIMESTAMP,
+    modified_at       TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS lesson
@@ -94,16 +108,20 @@ CREATE TABLE IF NOT EXISTS lesson
     pay_type    VARCHAR(64),
     lesson_type VARCHAR(64),
     description VARCHAR(128),
-    cost        NUMERIC(10, 2)
+    cost        NUMERIC(10, 2),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS comment
 (
-    order_id  INT REFERENCES orders (id),
-    lesson_id INT REFERENCES lesson (id),
-    text      VARCHAR(256),
-    added_at  TIMESTAMP(0) NOT NULL,
-    PRIMARY KEY (text, added_at)
+    order_id    INT REFERENCES orders (id),
+    lesson_id   INT REFERENCES lesson (id),
+    text        VARCHAR(256),
+    added_at    TIMESTAMP(0) NOT NULL,
+    PRIMARY KEY (text, added_at),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS teacher_subject
@@ -115,11 +133,13 @@ CREATE TABLE IF NOT EXISTS teacher_subject
 
 CREATE TABLE IF NOT EXISTS salary_log
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    payment    NUMERIC(10, 2),
-    added_at   TIMESTAMP(0),
-    teacher_id INT REFERENCES teacher (id),
-    admin_id   INT REFERENCES admin (id)
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    payment     NUMERIC(10, 2),
+    added_at    TIMESTAMP(0),
+    teacher_id  INT REFERENCES teacher (id),
+    admin_id    INT REFERENCES admin (id),
+    saved_at    TIMESTAMP,
+    modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS task
@@ -130,7 +150,9 @@ CREATE TABLE IF NOT EXISTS task
     description       VARCHAR(64) NOT NULL,
     created_date_time TIMESTAMP   NOT NULL,
     end_date_time     TIMESTAMP   NOT NULL,
-    task_Type         VARCHAR(64) NOT NULL
+    task_Type         VARCHAR(64) NOT NULL,
+    saved_at          TIMESTAMP,
+    modified_at       TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS student_lesson
