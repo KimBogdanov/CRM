b402a83a045b173c35277a8349625aa5eca825e5
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-03-06 18:03:18 +0800

    feat: change LogInfoService

--- src/main/java/ru/crm/system/service/LogInfoService.java
+++ src/main/java/ru/crm/system/service/LogInfoService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.crm.system.database.entity.Abonement;
 import ru.crm.system.database.entity.Lesson;
 import ru.crm.system.database.entity.LogInfo;
+import ru.crm.system.database.entity.Student;
 import ru.crm.system.database.entity.enums.ActionType;
 import ru.crm.system.database.repository.LogInfoRepository;
 import ru.crm.system.dto.abonement.AbonementCreatEditDto;
@@ -13,10 +14,11 @@ import ru.crm.system.dto.loginfo.LogInfoCreateDto;
 import ru.crm.system.mapper.LogInfoCreateMapper;
 
 import java.math.BigDecimal;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 
 import static java.time.LocalDateTime.now;
-import static java.time.temporal.ChronoUnit.MINUTES;
 import static java.time.temporal.ChronoUnit.SECONDS;
 
 @Service
@@ -50,7 +52,6 @@ public class LogInfoService {
     /**
      * Method for creating {@link ru.crm.system.database.entity.LogInfo} when creating and selling new {@link ru.crm.system.database.entity.Abonement}.
      */
-
     public LogInfoCreateDto createAbonementLogInfo(AbonementCreatEditDto createDto,
                                                    Integer adminId,
                                                    Integer orderId) {
@@ -86,54 +87,61 @@ public class LogInfoService {
      * Method for creating {@link ru.crm.system.database.entity.LogInfo} when saving new lesson into db with
      * {@link ru.crm.system.database.entity.enums.ActionType#ATTENDING_A_LESSON}
      */
-    public LogInfoCreateDto createLogInfoWhenLessonAppointed(Integer adminId, Lesson lesson) {
+    public LogInfoCreateDto createLogInfoWhenLessonAppointed(Lesson lesson) {
         return LogInfoCreateDto.builder()
                 .action(ActionType.ATTENDING_A_LESSON)
                 .description(String
-                        .format("%s назначен новый урок ученику: %s %s. Учитель: %s %s. Предмет: %s",
-                                lesson.getDateTime().truncatedTo(MINUTES),
-                                lesson.getStudent().getUserInfo().getFirstName(),
-                                lesson.getStudent().getUserInfo().getLastName(),
+                        .format("На %s %s назначен новый урок №%s для студентов: %s. Учитель: %s %s. Предмет: %s",
+                                lesson.getDate(),
+                                lesson.getTime().truncatedTo(ChronoUnit.MINUTES),
+                                lesson.getId(),
+                                lesson.getStudents().stream()
+                                        .map(student -> String.format("%s %s",
+                                                student.getUserInfo().getFirstName(),
+                                                student.getUserInfo().getLastName())).toList(),
                                 lesson.getTeacher().getUserInfo().getFirstName(),
                                 lesson.getTeacher().getUserInfo().getLastName(),
                                 lesson.getSubject().getName()))
                 .createdAt(now().truncatedTo(SECONDS))
-                .adminId(adminId)
-                .studentId(lesson.getStudent().getId())
+                .teacherId(lesson.getTeacher().getId())
+                .lessonId(lesson.getId())
                 .build();
     }
 
     /**
      * Method for creating {@link ru.crm.system.database.entity.LogInfo} when withdrawing money from a student's abonement
      */
-    public LogInfoCreateDto createLogInfoWhenWriteOffFromStudentBalance(Lesson lesson) {
-        return LogInfoCreateDto.builder()
-                .action(ActionType.WRITE_OFF_FROM_STUDENT_BALANCE)
-                .description(String
-                        .format("С баланса ученика %s %s было списано %s руб.",
-                                lesson.getStudent().getUserInfo().getFirstName(),
-                                lesson.getStudent().getUserInfo().getLastName(),
-                                lesson.getCost()))
-                .createdAt(now().truncatedTo(SECONDS))
-                .teacherId(lesson.getTeacher().getId())
-                .studentId(lesson.getStudent().getId())
-                .build();
+    public List<LogInfoCreateDto> createLogInfosWhenWriteOffFromStudentsBalance(Lesson lesson) {
+        return lesson.getStudents().stream()
+                .map(student -> LogInfoCreateDto.builder()
+                        .action(ActionType.WRITE_OFF_FROM_STUDENT_BALANCE)
+                        .description(String
+                                .format("С баланса ученика %s %s было списано %s руб.",
+                                        student.getUserInfo().getFirstName(),
+                                        student.getUserInfo().getLastName(),
+                                        lesson.getCost()))
+                        .createdAt(now().truncatedTo(SECONDS))
+                        .teacherId(lesson.getTeacher().getId())
+                        .build())
+                .toList();
     }
 
     /**
      * Method for creating {@link ru.crm.system.database.entity.LogInfo} when the lesson is canceled for a valid reason.
      */
-    public LogInfoCreateDto createLogInfoWhenLessonCanceledByGoodReason(Lesson lesson) {
-        return LogInfoCreateDto.builder()
-                .action(ActionType.MISSING_CLASS)
-                .description(String
-                        .format("Ученик %s %s пропустил занятие по уважительной причине.",
-                                lesson.getStudent().getUserInfo().getFirstName(),
-                                lesson.getStudent().getUserInfo().getLastName()))
-                .createdAt(now().truncatedTo(SECONDS))
-                .teacherId(lesson.getTeacher().getId())
-                .studentId(lesson.getStudent().getId())
-                .build();
+    public List<LogInfoCreateDto> createLogInfoWhenLessonCanceledByGoodReason(Lesson lesson) {
+        return lesson.getStudents().stream()
+                .map(student -> LogInfoCreateDto.builder()
+                        .action(ActionType.MISSING_CLASS)
+                        .description(String
+                                .format("Ученик %s %s пропустил занятие по уважительной причине.",
+                                        student.getUserInfo().getFirstName(),
+                                        student.getUserInfo().getLastName()))
+                        .createdAt(now().truncatedTo(SECONDS))
+                        .teacherId(lesson.getTeacher().getId())
+                        .studentId(student.getId())
+                        .build())
+                .toList();
     }
 
     /**
@@ -148,7 +156,6 @@ public class LogInfoService {
                         lesson.getId(),
                         text))
                 .createdAt(now().truncatedTo(SECONDS))
-                .studentId(lesson.getStudent().getId())
                 .teacherId(lesson.getTeacher().getId())
                 .build();
     }
@@ -162,7 +169,6 @@ public class LogInfoService {
                 .description("Занятие было отменено.")
                 .createdAt(now().truncatedTo(SECONDS))
                 .teacherId(lesson.getTeacher().getId())
-                .studentId(lesson.getStudent().getId())
                 .build();
     }
 
@@ -178,7 +184,6 @@ public class LogInfoService {
                                 lesson.getTeacher().getUserInfo().getLastName()))
                 .createdAt(now().truncatedTo(SECONDS))
                 .teacherId(lesson.getTeacher().getId())
-                .studentId(lesson.getStudent().getId())
                 .build();
     }
 
@@ -195,7 +200,24 @@ public class LogInfoService {
                                 salaryService.getPaymentForLesson(lesson)))
                 .createdAt(now().truncatedTo(SECONDS))
                 .teacherId(lesson.getTeacher().getId())
-                .studentId(lesson.getStudent().getId())
+                .build();
+    }
+
+    /**
+     * Method to create LogInfo when saving a new student to the database with
+     * status TRANSFER_TO_STUDENT from {@link ru.crm.system.database.entity.enums.ActionType}.
+     */
+    public LogInfoCreateDto createStudentLogInfo(Integer adminId, Integer oderId, Student student) {
+        return LogInfoCreateDto.builder()
+                .action(ActionType.TRANSFER_TO_STUDENT)
+                .description(String.format("Добавлен новый ученик - %s %s из заказа №%d",
+                        student.getUserInfo().getFirstName(),
+                        student.getUserInfo().getLastName(),
+                        oderId))
+                .createdAt(now().truncatedTo(SECONDS))
+                .orderId(oderId)
+                .adminId(adminId)
+                .studentId(student.getId())
                 .build();
     }
 }
