9472799530ec43b3ff68d2991d8ebec5eaab0045
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-03-09 17:08:21 +0800

    feat: add methods to find logs by teacher and students

--- src/main/java/ru/crm/system/database/repository/LogInfoRepository.java
+++ src/main/java/ru/crm/system/database/repository/LogInfoRepository.java
@@ -13,5 +13,19 @@ public interface LogInfoRepository extends JpaRepository<LogInfo, Integer> {
             from LogInfo li
             where li.lesson.id = :id
             """)
-    List<LogInfo> findByLessonId(Integer id);
+    List<LogInfo> findAllByLessonId(Integer id);
+
+    @Query("""
+            select li
+            from LogInfo li
+            where li.teacher.id = :id
+            """)
+    List<LogInfo> findAllByTeacherId(Integer id);
+
+    @Query("""
+            select li
+            from LogInfo li
+            where li.student.id in :ids
+            """)
+    List<LogInfo> findAllByStudentIds(Integer... ids);
 }
