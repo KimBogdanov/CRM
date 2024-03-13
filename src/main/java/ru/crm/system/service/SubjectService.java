9309bf20aed262e466e69a0f486e116f691f2dfe
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-03-13 17:23:03 +0800

    feat: refactor and change SubjectService

--- src/main/java/ru/crm/system/service/SubjectService.java
+++ src/main/java/ru/crm/system/service/SubjectService.java
@@ -6,10 +6,11 @@ import ru.crm.system.database.entity.Subject;
 import ru.crm.system.database.repository.SubjectRepository;
 import ru.crm.system.dto.subject.SubjectCreateEditDto;
 import ru.crm.system.dto.subject.SubjectReadDto;
+import ru.crm.system.exception.CreateEntityException;
 import ru.crm.system.mapper.subject.SubjectCreateEditMapper;
 import ru.crm.system.mapper.subject.SubjectReadMapper;
 
-import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityExistsException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,68 +22,41 @@ public class SubjectService {
     private final SubjectCreateEditMapper subjectCreateEditMapper;
     private final SubjectReadMapper subjectReadMapper;
 
-    /**
-     * Возвращает список всех предметов
-     *
-     * @return список всех предметов
-     */
-    public List<SubjectReadDto> getAll() {
+    public List<String> findAllSubjectNames() {
         return subjectRepository.findAll().stream()
-                .map(subjectReadMapper::map)
+                .map(Subject::getName)
                 .toList();
     }
 
-    /**
-     * Возвращает список всех предметов по части названия
-     * например для выпадающего списка предметов
-     *
-     * @param name часть названия предмета
-     * @return список всех предметов
-     */
-    public List<Subject> findAllByNameContaining(String name) {
-        return subjectRepository.findAllByNameContaining(name);
+    public List<String> findAllByNameContaining(String name) {
+        return subjectRepository.findAllByNameContaining(name).stream()
+                .map(Subject::getName)
+                .toList();
     }
 
-    /**
-     * возвращает предмет по id
-     *
-     * @param id - id предмета
-     * @return предмет
-     */
-    public SubjectReadDto getById(Integer id) {
+    public Optional<SubjectReadDto> findById(Integer id) {
         return subjectRepository.findById(id)
-                .map(subjectReadMapper::map)
-                .orElseThrow(() -> new EntityNotFoundException("Предмет с id " + id + " не найден в базе данных"));
+                .map(subjectReadMapper::map);
     }
-
-//    /**
-//     * сохраняет предмет в БД или выдает ошибку, если он там уже есть
-//     *
-//     * @param subject предмет
-//     * @return сохраненный предмет, если сохранение удачно
-//     */
-//    public Subject save(Subject subject) {
-//
-//        if (subjectRepository.existsByNameLikeIgnoreCase(subject.getName())) {
-//            throw new EntityExistsException("Предмет " + subject.getName() + " уже есть в базе данных");
-//        }
-//        return subjectRepository.save(subject);
-//    }
 
     public SubjectReadDto save(SubjectCreateEditDto createDto) {
+        if (subjectRepository.existsByNameLikeIgnoreCase(createDto.name())) {
+            throw new EntityExistsException("Предмет " + createDto.name() + " уже есть в базе данных");
+        }
         return Optional.of(createDto)
                 .map(subjectCreateEditMapper::map)
                 .map(subjectRepository::save)
                 .map(subjectReadMapper::map)
-                .orElseThrow();
+                .orElseThrow(() -> new CreateEntityException("Невозможно создать предмет с указанными параметрами"));
     }
 
-    /**
-     * Удаляет предмет из БД по id
-     *
-     * @param id id предмета
-     */
-    public void delete(int id) {
-        subjectRepository.deleteById(id);
+    public boolean delete(Integer id) {
+        return subjectRepository.findById(id)
+                .map(subject -> {
+                    subjectRepository.deleteById(id);
+                    subjectRepository.flush();
+                    return true;
+                })
+                .orElse(false);
     }
 }
