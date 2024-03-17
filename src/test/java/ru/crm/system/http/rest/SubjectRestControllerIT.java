e09aac4aa81c2aceef162df176988c9c63d6fbaf
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-03-17 11:19:12 +0800

    test: add IT to check find subject by ID

--- src/test/java/ru/crm/system/http/rest/SubjectRestControllerIT.java
+++ src/test/java/ru/crm/system/http/rest/SubjectRestControllerIT.java
@@ -3,17 +3,19 @@ package ru.crm.system.http.rest;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.crm.system.integration.IntegrationTestBase;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RequiredArgsConstructor
-@AutoConfigureMockMvc
 class SubjectRestControllerIT extends IntegrationTestBase {
 
     private static final String SUBJECT_BASE_URL = "/api/v1/subjects";
@@ -32,5 +34,20 @@ class SubjectRestControllerIT extends IntegrationTestBase {
     void findAllBySubjectName_shouldReturn200() throws Exception {
         mockMvc.perform(get(SUBJECT_BASE_URL))
                 .andExpect(status().isOk());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Вокал", "Гитара", "Скрипка", "Ударные", "Фортепиано"})
+    void findByName_shouldFindSubjectByNameAndReturn200_whenSubjectExists(String name) throws Exception {
+        mockMvc.perform(get(SUBJECT_BASE_URL + "/by-name").queryParam("name", name))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Барабан", "Флейта", "Балалайка", "Тромбон"})
+    void findByName_shouldReturn404_whenSubjectNotExist(String name) throws Exception {
+        mockMvc.perform(get(SUBJECT_BASE_URL + "/by-name").queryParam("name", name))
+                .andExpect(status().isNotFound());
     }
 }
