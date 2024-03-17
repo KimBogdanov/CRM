0ec9796c16a2b4876f48dd84f35cf370878d3271
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-03-17 11:18:47 +0800

    feat: add findById at SubjectRestController

--- src/main/java/ru/crm/system/http/rest/SubjectRestController.java
+++ src/main/java/ru/crm/system/http/rest/SubjectRestController.java
@@ -1,13 +1,19 @@
 package ru.crm.system.http.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.crm.system.dto.subject.SubjectReadDto;
+import ru.crm.system.exception.NotFoundException;
 import ru.crm.system.service.SubjectService;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/v1/subjects")
@@ -20,8 +26,15 @@ public class SubjectRestController {
     public ResponseEntity<List<String>> findAllSubjectNames() {
         var subjectNames = subjectService.findAllSubjectNames();
 
-        return ResponseEntity
-                .ok()
-                .body(subjectNames);
+        return ok().body(subjectNames);
+    }
+
+    @GetMapping("/by-name")
+    public ResponseEntity<SubjectReadDto> findByName(@RequestParam String name) {
+        return subjectService.findByName(name)
+                .map(subject -> ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(subject)
+                ).orElseThrow(() -> new NotFoundException("Предмет с названием " + name + " не найден в базе данных."));
     }
 }
